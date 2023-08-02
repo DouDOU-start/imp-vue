e9a2b2106a05836f64934ee4ec1284702877e5e8
allen-MacAir <allen.huang@hanglok-tech.cn>
2023-08-02 16:04:44 +0800

    新增上传标注文件、删除标注文件

--- src/api/file.ts
+++ src/api/file.ts
@@ -1,13 +1,21 @@
-import {DOWNLOAD, DOWNLOADSERIES, POST} from "@/api/api";
+import {DEL, DOWNLOAD, DOWNLOADSERIES, POST} from "@/api/api";
 
 export async function uploadDicomApi(file: FormData) {
     return await POST('/file/dicom', file);
+}
+
+export async function uploadLabelApi(seriesId: string, file: FormData) {
+    return await POST(`/file/label/${seriesId}`, file);
 }
 
 export async function downLoadSeriesApi(seriesId: number) {
     await DOWNLOADSERIES(`/file/series/${seriesId}`, null);
 }
 
+export async function delSeriesLabelApi(seriesId: string, fileName: string) {
+    await DEL(`/file/label/${seriesId}/${fileName}`, null);
+}
+
 export async function downLoadSeriesLabelApi(fileLocation: string) {
-    await DOWNLOAD(`/file/label/${fileLocation}`, null);
+    await DOWNLOAD(`/file/label/${fileLocation}`);
 }
--- src/views/SeriesDetail.vue
+++ src/views/SeriesDetail.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import {useRouter} from 'vue-router';
-import {getSeriesDetail, getSeriesInstance, GetSeriesLabel, getSeriesLabel} from "@/api/imp";
-import type {Ref} from 'vue'
-import type {GetSeriesDetail, GetSeriesInstance} from '@/api/imp'
-import {downLoadSeriesLabelApi} from "@/api/file";
-
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { getSeriesDetail, getSeriesInstance, type GetSeriesLabel, getSeriesLabel } from "@/api/imp";
+import type { Ref } from 'vue'
+import type { GetSeriesDetail, GetSeriesInstance } from '@/api/imp'
+import { delSeriesLabelApi, downLoadSeriesLabelApi, uploadLabelApi } from "@/api/file";
+import { ElNotification } from "element-plus";
 
 const seriesDetailLabel = {
   "seriesDescription": "系列描述",
@@ -62,28 +62,62 @@ function init() {
 const $router = useRouter();
 
 async function loadSeriesDetail() {
-  const {data} = await getSeriesDetail($router.currentRoute.value.params.seriesId as string)
+  const { data } = await getSeriesDetail($router.currentRoute.value.params.seriesId as string)
   seriesDetail.value = data
   seriesDetail.value.shapeInfo = seriesDetail.value.row + ' * ' + seriesDetail.value.columns + ' * ' + seriesDetail.value.instanceNum
   seriesDetail.value.sliceInfo = seriesDetail.value.pixelSpacing + ' * ' + seriesDetail.value.sliceThickness
 }
 
 async function loadSeriesLabel() {
-  const {data} = await getSeriesLabel($router.currentRoute.value.params.seriesId as string)
+  const { data } = await getSeriesLabel($router.currentRoute.value.params.seriesId as string)
   seriesLabel.value = data
 }
 
 async function loadSeriesInstance() {
-  const {data} = await getSeriesInstance($router.currentRoute.value.params.seriesId as string)
+  const { data } = await getSeriesInstance($router.currentRoute.value.params.seriesId as string)
   seriesInstance.value = data
   seriesInstance.value.forEach(instance => {
     instance.imageUrl = '/api/file/jpg/' + instance.id
   })
 }
 
+// 删除标签
+function delLabel(fileName: string) {
+  delSeriesLabelApi($router.currentRoute.value.params.seriesId as string, fileName)
+}
+
 function downloadLabel(val: any) {
   downLoadSeriesLabelApi(val)
-  console.log(val)
+}
+
+const upload = ref<any>()
+
+// 上传标签文件
+function uploadLabel(param: any) {
+
+  const formData = new FormData()
+  formData.append('file', param.file)
+
+  uploadLabelApi($router.currentRoute.value.params.seriesId as string, formData).then(res => {
+    console.log(`上传标签文件成功，${res}`)
+    ElNotification({
+      message: `${param.file.name} 上传成功！`,
+      type: 'success',
+    })
+
+  }).catch(res => {
+    console.error(`上传标签文件失败，${res}`)
+    ElNotification({
+      message: `${param.file.name} 上传失败，${res.response.data.msg}`,
+      type: 'error',
+    })
+  })
+
+}
+
+// 文件发生改变
+function fileChange() {
+  upload.value.clearFiles() //清除文件对象
 }
 
 onMounted(() => {
@@ -92,24 +126,29 @@ onMounted(() => {
 </script>
 
 <template>
-
   <el-descriptions title="系列详情">
     <el-descriptions-item v-for="(value, key) in seriesDetailLabel" :key="key" :label="value">{{ seriesDetail[key] }}
     </el-descriptions-item>
   </el-descriptions>
 
   <div class="label-list">
+
+    <!--        上传标注文件-->
+    <el-upload class="upload-dicom" ref="upload" action="string" :before-upload="onBeforeUploadDicom"
+      :http-request="uploadLabel" :on-change="fileChange" multiple>
+      <el-button size="small" type="primary">上传标签文件</el-button>
+    </el-upload>
+
+    <!-- 标注文件列表 -->
     <el-table :data="seriesLabel" style="width: 100%" max-height="250">
-      <el-table-column fixed prop="fileName" label="标注文件" width="150"/>
-      <el-table-column fixed prop="createdAt" label="上传时间" width="200"/>
+      <el-table-column fixed prop="fileName" label="标注文件" width="150" />
+      <el-table-column fixed prop="createdAt" label="上传时间" width="200" />
       <el-table-column label="" width="120">
         <template #default="scope">
-          <el-button
-              link
-              type="primary"
-              size="small"
-              @click.prevent="downloadLabel(scope.row.fileLocation)"
-          >
+          <el-button link type="primary" size="small" @click.prevent="delLabel(scope.row.fileName)">
+            删除
+          </el-button>
+          <el-button link type="primary" size="small" @click.prevent="downloadLabel(scope.row.fileLocation)">
             下载
           </el-button>
         </template>
@@ -125,11 +164,10 @@ onMounted(() => {
           <br>
           Slice Location: {{ item.sliceLocation }}
         </div>
-        <el-image class="img" :src="item.imageUrl"/>
+        <el-image class="img" :src="item.imageUrl" />
       </div>
     </div>
   </div>
-
 </template>
 
 
@@ -157,9 +195,9 @@ onMounted(() => {
 .image-container {
   position: relative;
   justify-content: center;
-  align-items: center; /* 垂直居中 */
-  width: 300px; /* 调整div宽度 */
-  height: 300px; /* 调整div高度 */
+  align-items: center;
+  width: 300px;
+  height: 300px;
   border: 1px solid #ccc;
   float: left;
 
